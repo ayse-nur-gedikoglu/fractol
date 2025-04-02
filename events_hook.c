925eeede063542acadb523436c7250f03d36e4d6
Ayse Nur Gedikoglu <gedikoglu_27@icloud.com>
2025-04-02 13:05:21 +0300

    added

--- events_hook.c
+++ events_hook.c
@@ -1,22 +1,51 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   events_hook.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agedikog <agedikog@student.42.fr>          +#+  +:+       +#+        */
+/*   By: agedikog <gedikoglu_27@icloud.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 21:27:55 by agedikog          #+#    #+#             */
-/*   Updated: 2025/03/22 03:14:46 by agedikog         ###   ########.fr       */
+/*   Updated: 2025/04/02 12:53:56 by agedikog         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "fractol.h"
 
-static void	process_movement(int keycode, t_fractol *f)
+static void	zoom(t_fractol *f, int x, int y, int direction)
 {
-	double	shift;
+	double	mouse_re;
+	double	mouse_im;
+	double	zoom;
 
-	shift = 0.1 * (f->max_real - f->min_real);
+	if (direction == 1)
+		zoom = 0.9;
+	else
+		zoom = 1.1;
+	mouse_re = f->min_real + ((double)x / WIDTH) * (f->max_real - f->min_real);
+	mouse_im = f->min_imag + ((double)y / HEIGHT) * (f->max_imag - f->min_imag);
+	f->min_real = mouse_re + (f->min_real - mouse_re) * zoom;
+	f->max_real = mouse_re + (f->max_real - mouse_re) * zoom;
+	f->min_imag = mouse_im + (f->min_imag - mouse_im) * zoom;
+	f->max_imag = mouse_im + (f->max_imag - mouse_im) * zoom;
+}
+
+int	mouse_hook(int button, int x, int y, t_fractol *f)
+{
+	if (button == ZOOM_IN)
+		zoom(f, x, y, 1);
+	else if (button == ZOOM_OUT)
+		zoom(f, x, y, -1);
+	else
+		return (0);
+	initialize_draw(f);
+	return (0);
+}
+
+static void	handle_movement(int keycode, t_fractol *f)
+{
+	double shift = 0.1 * (f->max_real - f->min_real);
+
 	if (keycode == MOVE_LEFT)
 	{
 		f->min_real -= shift;
@@ -39,15 +68,12 @@ static void	process_movement(int keycode, t_fractol *f)
 	}
 }
 
-static void	process_settings(int keycode, t_fractol *f)
+static void	handle_settings(int keycode, t_fractol *f)
 {
 	if (keycode == INCREASE_ITER)
 		f->max_iter += 10;
-	else if (keycode == DECREASE_ITER)
-	{
-		if (f->max_iter > 10)
-			f->max_iter -= 10;
-	}
+	else if (keycode == DECREASE_ITER && f->max_iter > 10)
+		f->max_iter -= 10;
 	else if (keycode == RESET_VIEW)
 		reset_view(f);
 	else if (keycode == COLOR_SHIFT)
@@ -57,35 +83,9 @@ static void	process_settings(int keycode, t_fractol *f)
 int	key_hook(int keycode, t_fractol *f)
 {
 	if (keycode == EXIT_KEY)
-	{
 		close_window(f);
-		return (0);
-	}
-	process_movement(keycode, f);
-	process_settings(keycode, f);
-	initialize_draw(f);
-	mlx_put_image_to_window(f->mlx, f->mlx_win, f->canvas->img, 0, 0);
-	return (0);
-}
-
-int	mouse_hook(int button, int x, int y, t_fractol *f)
-{
-	double	mouse_re;
-	double	mouse_im;
-	double	factor;
-
-	mouse_re = f->min_real + ((double)x / WIDTH) * (f->max_real - f->min_real);
-	mouse_im = f->min_imag + ((double)y / HEIGHT) * (f->max_imag - f->min_imag);
-	if (button == ZOOM_IN)
-		factor = 0.9;
-	else if (button == ZOOM_OUT)
-		factor = 1.1;
-	else
-		return (0);
-	f->min_real = mouse_re + (f->min_real - mouse_re) * factor;
-	f->max_real = mouse_re + (f->max_real - mouse_re) * factor;
-	f->min_imag = mouse_im + (f->min_imag - mouse_im) * factor;
-	f->max_imag = mouse_im + (f->max_imag - mouse_im) * factor;
+	handle_movement(keycode, f);
+	handle_settings(keycode, f);
 	initialize_draw(f);
 	return (0);
 }
